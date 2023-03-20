4811728f6ecfc18d8b1492176a9ba27e2b29229b
王 <22379099@qq.com>
2023-03-20 17:51:37 +0800

    下载账单接口

---
+++ src/WWB.Paylink.BaoFooPay/Domain/ResResult/TransactionDownResponseResult.cs
@@ -0,0 +1,12 @@
+﻿namespace WWB.Paylink.BaoFooPay.Domain.ResResult
+{
+    public class TransactionDownResponseResult
+    {
+        public string batchDate { get; set; }
+        public string downloadUrl { get; set; }
+        public string infoNo { get; set; }
+        public string orderStatus { get; set; }
+        public string respCode { get; set; }
+        public string respMsg { get; set; }
+    }
+}
---
+++ src/WWB.Paylink.BaoFooPay/Request/TransactionDownRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using WWB.Paylink.BaoFooPay.Response;
+
+namespace WWB.Paylink.BaoFooPay.Request
+{
+    public class TransactionDownRequest : AbstractRequest, IBaoFooPayRequest<TransactionDownResponse>
+    {
+        public string batchDate { get; set; }
+        public string billType { get; set; }
+
+        #region 方法
+
+        protected override string GetMethod()
+        {
+            return "QUERY_TRADE_FILE";
+        }
+
+        public string GetRequestUrl(bool debug)
+        {
+            return debug ? "https://test-api.huishouqian.com/api/acquiring" : "https://api.huishouqian.com/api/acquiring";
+        }
+
+        public IDictionary<string, string> PrimaryHandler(BaoFooPayOptions options)
+        {
+            return PrimaryHandler(this, options);
+        }
+
+        #endregion 方法
+    }
+}
