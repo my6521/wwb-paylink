7a8883654fe94ced29a4e73642f7119bccfebab4
王 <22379099@qq.com>
2023-03-29 17:10:06 +0800

    删除冗余

--- src/WWB.Paylink.BaoFooPay/Constants/OrderStatus.cs
+++ src/WWB.Paylink.BaoFooPay/Constants/OrderStatus.cs
@@ -2,9 +2,24 @@
 {
     public class OrderStatus
     {
+        /// <summary>
+        /// 初始化
+        /// </summary>
         public const string INIT = "INIT";
+
+        /// <summary>
+        /// 处理中
+        /// </summary>
         public const string PROCESSING = "PROCESSING";
+
+        /// <summary>
+        /// 成功
+        /// </summary>
         public const string SUCCESS = "SUCCESS";
+
+        /// <summary>
+        /// 失败
+        /// </summary>
         public const string FAIL = "FAIL";
     }
 }
