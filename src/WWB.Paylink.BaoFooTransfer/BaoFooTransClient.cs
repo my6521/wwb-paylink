41c3009b378de79ad6ca48cac12b3059c463aaab
wwb <22379099@qq.com>
2024-05-09 13:41:19 +0800

    log

--- src/WWB.Paylink.BaoFooTransfer/BaoFooTransClient.cs
+++ src/WWB.Paylink.BaoFooTransfer/BaoFooTransClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WWB.Paylink.BaoFooTransfer.Parser;
@@ -7,42 +8,55 @@ using WWB.Paylink.Utility.Security;
 
 namespace WWB.Paylink.BaoFooTransfer
 {
-    public class BaoFooTransClient : IBaoFooTransClient
+  public class BaoFooTransClient : IBaoFooTransClient
+  {
+    public const string Name = "WWB.Paylink.BaoFooTransfer.Client";
+
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ILogger<BaoFooTransClient> _logger;
+
+    public BaoFooTransClient(IHttpClientFactory httpClientFactory, ILogger<BaoFooTransClient> logger)
     {
-        public const string Name = "WWB.Paylink.BaoFooTransfer.Client";
-
-        private readonly IHttpClientFactory _httpClientFactory;
-
-        public BaoFooTransClient(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
-
-        public async Task<T> ExecuteAsync<T>(IBaoFooTransRequest<T> request, BaoFooTransOptions options) where T : BaseResponse
-        {
-            if (options == null)
-            {
-                throw new ArgumentNullException(nameof(options));
-            }
-
-            //获取参数
-            var txtParams = request.PrimaryHandler(options);
-            //获取请求url
-            var url = request.GetRequestUrl(options.Debug);
-            //获取提交类型
-            var contentType = request.GetContentType();
-            //提交
-            using (var client = _httpClientFactory.CreateClient(Name))
-            {
-                var (body, isSuccessStatusCode) = await client.PostAsync(url, contentType, txtParams);
-                //解密
-                var realContent = RSAUtil.DecryptByCer(body, options.CerCertificate);
-                //反序列化
-                var parser = new ResponseJsonParser<T>();
-                var response = parser.Parse(realContent);
-
-                return response;
-            }
-        }
+      _httpClientFactory = httpClientFactory;
+      _logger = logger;
     }
+
+    public async Task<T> ExecuteAsync<T>(IBaoFooTransRequest<T> request, BaoFooTransOptions options) where T : BaseResponse
+    {
+      if (options == null)
+      {
+        throw new ArgumentNullException(nameof(options));
+      }
+
+      //获取参数
+      var txtParams = request.PrimaryHandler(options);
+      //获取请求url
+      var url = request.GetRequestUrl(options.Debug);
+      //获取提交类型0
+      var contentType = request.GetContentType();
+      //提交
+      using (var client = _httpClientFactory.CreateClient(Name))
+      {
+        var (body, isSuccessStatusCode) = await client.PostAsync(url, contentType, txtParams);
+        if (isSuccessStatusCode)
+        {
+          _logger.LogInformation($"反馈消息：{body}");
+
+          //解密
+          var realContent = RSAUtil.DecryptByCer(body, options.CerCertificate);
+
+          _logger.LogInformation($"消息解密：{realContent}");
+
+          //反序列化
+          var parser = new ResponseJsonParser<T>();
+          var response = parser.Parse(realContent);
+          return response;
+        }
+        else
+        {
+          throw new BaoFooTransException($"请求错误");
+        }
+      }
+    }
+  }
 }
